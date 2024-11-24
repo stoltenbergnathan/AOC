abf5a5beabfcc5e0106acb68818b5fed077220ae
Nathan Stoltenberg <nds4@pdx.edu>
2024-11-23 19:44:06 -0800

    finished 20

--- 2023/Day20/main.py
+++ 2023/Day20/main.py
@@ -1,4 +1,24 @@
 from enum import Enum
+from typing import Tuple
+
+low_pulses_sent: int = 0
+high_pulses_sent: int = 0
+
+num_butt_pressed: int = 0
+
+
+class ModuleQueue:
+    queue: list[Tuple["Module", "Pulse", "Module"]] = []
+
+    @classmethod
+    def add(cls, mod: "Module", pulse: "Pulse", parent: "Module") -> None:
+        cls.queue.append((mod, pulse, parent))
+
+    @classmethod
+    def take(cls) -> Tuple["Module", "Pulse", "Module"] | None:
+        if len(cls.queue) != 0:
+            return cls.queue.pop(0)
+        return None
 
 
 class Pulse(Enum):
@@ -6,8 +26,17 @@ class Pulse(Enum):
     high = 1
 
 
+def count_pulse(pulse: Pulse) -> None:
+    global high_pulses_sent
+    global low_pulses_sent
+
+    if pulse == Pulse.high:
+        high_pulses_sent += 1
+    else:
+        low_pulses_sent += 1
+
+
 class State(Enum):
-    unknown = -1
     off = 0
     on = 1
 
@@ -23,7 +52,10 @@ class Module:
         self.destination_modules = destination_modules
 
     def input(self, pulse: Pulse, sending_module: "Module") -> None:
-        pass
+        if self.name == "df" and pulse == Pulse.high and sending_module.name == "xl":
+            print("BREAK: ", num_butt_pressed)
+            exit()
+        # print(sending_module.name + " -" + pulse.name + "-> " + self.name)
 
 
 class FlipFlop(Module):
@@ -32,6 +64,8 @@ class FlipFlop(Module):
         self.state: State = State.off
 
     def input(self, pulse: Pulse, sending_module: Module):
+        super().input(pulse, sending_module)
+
         if pulse == Pulse.high:
             return
 
@@ -44,28 +78,29 @@ class FlipFlop(Module):
         self.state = self.state.toggle()
 
         for module in self.destination_modules:
-            module.input(pulse_to_send, self)
+            count_pulse(pulse_to_send)
+            ModuleQueue.add(module, pulse_to_send, self)
 
 
 class Conjunction(Module):
     def __init__(self, name: str, destination_modules: list[Module]) -> None:
         super().__init__(name, destination_modules)
-        self.state: State = State.unknown
-        self.last_pulse_from_desitnations: dict[Module, Pulse]
-        for module in self.destination_modules:
-            self.last_pulse_from_desitnations[module] = Pulse.low
+        self.last_pulse_from_desitnations: dict[Module, Pulse] = {}
 
     def input(self, pulse: Pulse, sending_module: Module):
+        super().input(pulse, sending_module)
+
         self.last_pulse_from_desitnations[sending_module] = pulse
 
         pulse_to_send: Pulse
-        if self.last_pulse_from_desitnations.values().__contains__(Pulse.low):
+        if Pulse.low in self.last_pulse_from_desitnations.values():
             pulse_to_send = Pulse.high
         else:
             pulse_to_send = Pulse.low
 
         for module in self.destination_modules:
-            module.input(pulse_to_send, self)
+            count_pulse(pulse_to_send)
+            ModuleQueue.add(module, pulse_to_send, self)
 
 
 class Broadcaster(Module):
@@ -73,21 +108,28 @@ class Broadcaster(Module):
         super().__init__(name, destination_modules)
 
     def input(self, pulse: Pulse, sending_module: Module) -> None:
+        super().input(pulse, sending_module)
+
         for module in self.destination_modules:
-            module.input(pulse, self)
+            count_pulse(pulse)
+            ModuleQueue.add(module, pulse, self)
 
 
-def get_init_modules_from_input() -> list[Module]:
+def get_modules_from_input() -> list[Module]:
     lines: list[str]
-    found_modules: list[Module] = []
+    modules: dict[Module, list[str]] = {}
     with open("./input.txt") as f:
         lines = f.readlines()
 
     for line in lines:
         splits: list[str] = line.split("->")
         input_module: str = splits[0].replace(" ", "")
-        new_module: Module
+        destination_modules: list[str] = (
+            splits[1].rstrip("\n").replace(" ", "").split(",")
+        )
 
+        # Curent Module
+        new_module: Module
         if input_module == "broadcaster":
             new_module = Broadcaster(input_module, [])
         elif input_module.startswith("%"):
@@ -97,12 +139,47 @@ def get_init_modules_from_input() -> list[Module]:
         else:
             raise Exception("Cannot convert into a module: ", input_module)
 
-        found_modules.append(new_module)
+        # Destination Modules
+        modules[new_module] = destination_modules
 
-    return found_modules
+    # Poplulate destination modules
+    for key, value in modules.items():
+        for module_str in value:
+            m = next((m for m in modules.keys() if m.name == module_str), None)
+            if m is not None:
+                key.destination_modules.append(m)
+            else:
+                key.destination_modules.append(Module(module_str, []))
+
+    # Populate Conjuction Modules
+    for module in [m for m in modules.keys() if type(m) == Conjunction]:
+        for mod in modules.keys():
+            if module in mod.destination_modules:
+                module.last_pulse_from_desitnations[mod] = Pulse.low
+
+    return list(modules.keys())
+
+
+def press_button(broadcaster_module: Module) -> None:
+    global low_pulses_sent
+
+    low_pulses_sent += 1
+    broadcaster_module.input(Pulse.low, Module("", []))
+
+    m: Tuple["Module", "Pulse", "Module"] | None = ModuleQueue.take()
+    while m is not None:
+        m[0].input(m[1], m[2])
+        m = ModuleQueue.take()
 
 
 if __name__ == "__main__":
-    mods = get_init_modules_from_input()
-    for mod in mods:
-        print(mod.name + " " + str(type(mod)))
+    mods = get_modules_from_input()
+    broadcaster_module = next(module for module in mods if module.name == "broadcaster")
+
+    while True:
+        num_butt_pressed += 1
+        press_button(broadcaster_module)
+        print(num_butt_pressed)
+
+    print("High pulses sent: ", high_pulses_sent)
+    print("Low pulses sent: ", low_pulses_sent)
