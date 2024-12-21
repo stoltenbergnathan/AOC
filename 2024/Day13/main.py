acb4e51bbb285c0aeb8a52d1351cc23304423525
Nathan Stoltenberg <stoltnate@gmail.com>
2024-12-21 10:18:00 -0800

    got wifi yay: part 2 day 13

--- 2024/Day13/main.py
+++ 2024/Day13/main.py
@@ -1,4 +1,10 @@
 from dataclasses import dataclass
+from typing import Any
+from sympy import Rational, Integer
+from sympy.abc import a, b
+from sympy.solvers import solve
+
+PART2: bool = True
 
 
 @dataclass
@@ -37,6 +43,10 @@ class ClawMachine:
             int(prize_str.split(",")[0][2:]), int(prize_str.split(",")[1][3:])
         )
 
+        if PART2:
+            self.prize.x += 10000000000000
+            self.prize.y += 10000000000000
+
     def tokens_to_solve(self) -> int:
         combonations: list[tuple[int, int]] = []
         for a in range(0, 101):
@@ -59,6 +69,20 @@ class ClawMachine:
             ]
         )
 
+    def tokens_to_solve2(self) -> int:
+        answer: dict[Any, Any] = solve(
+            [
+                a * self.buttona.x_change + b * self.buttonb.x_change - self.prize.x,
+                a * self.buttona.y_change + b * self.buttonb.y_change - self.prize.y,
+            ],
+            [a, b],
+        )
+
+        if type(answer[a]) is Rational or type(answer[b]) is Rational:
+            return -1
+
+        return self.buttona.cost(int(answer[a])) + self.buttonb.cost(int(answer[b]))
+
 
 def read_file(file_name: str) -> list[str]:
     with open(file_name) as f:
@@ -77,10 +101,16 @@ def main() -> None:
     ]
 
     tokens_used: int = 0
-    for claw_machine in claw_machines:
-        cost = claw_machine.tokens_to_solve()
-        if cost != -1:
-            tokens_used += cost
+    if not PART2:
+        for claw_machine in claw_machines:
+            cost = claw_machine.tokens_to_solve()
+            if cost != -1:
+                tokens_used += cost
+    else:
+        for claw_machine in claw_machines:
+            cost = claw_machine.tokens_to_solve2()
+            if cost != -1:
+                tokens_used += cost
 
     print(tokens_used)
 
